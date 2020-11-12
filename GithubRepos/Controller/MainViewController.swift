a9cbf63d325b03f2133a6a6900699613431fd63a
Yura Geyts <yura.geyts@gmail.com>
2020-11-12 15:52:31 +0200

    1.2.1
    added programming language choise

--- GithubRepos/Controller/MainViewController.swift
+++ GithubRepos/Controller/MainViewController.swift
@@ -7,18 +7,22 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     @IBOutlet weak var repositoryNameTextField: UITextField?
+    
+    var selectedLanguage = ""
+    var previousButton = UIButton()
     
     @IBAction func searchAction(_ sender: Any) {
         guard let reposName = repositoryNameTextField?.text else { return }
-        let urlString = "https://api.github.com/search/repositories?q=\(reposName)+language:swift&sort=stars&order=desc"
+        let urlString = generateURLwith(repositoryName: reposName, language: selectedLanguage)
+        
         let urlStringPage1 = "\(urlString)&per_page=15&page=1"
         let urlStringPage2 = "\(urlString)&per_page=15&page=2"
         
         let firstQueue = DispatchQueue(label: "firstQueue", qos: .utility)
         let secondQueue = DispatchQueue(label: "secondQueue", qos: .utility)
-
+        
         firstQueue.async {
             NetworkManager.shared.dataFetcher(urlString: urlStringPage1)
         }
@@ -26,5 +30,27 @@ class MainViewController: UIViewController {
             NetworkManager.shared.dataFetcher(urlString: urlStringPage2)
         }
         performSegue(withIdentifier: "showRepositoriesSegue", sender: nil)
+    }
+    
+    
+    @IBAction func selectedLanguage(_ sender: UIButton) {
+        
+        guard let selectedTitle = sender.currentTitle else { return }
+        
+        if previousButton != sender {
+            previousButton.isHighlighted = true
+            previousButton = sender
+            selectedLanguage = selectedTitle
+        } else {
+            if selectedLanguage == selectedTitle {
+                selectedLanguage = ""
+            } else {
+                selectedLanguage = selectedTitle
+            }
+        }
+    }
+    
+    private func generateURLwith(repositoryName: String, language: String) -> String {
+        return "https://api.github.com/search/repositories?q=\(repositoryName)+language:\(language)&sort=stars&order=desc"
     }
 }
